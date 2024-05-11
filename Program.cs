551d6b701abe7db435577351689f4ff30ee5327b
jasonbehrbair <jasonbairsfhs@gmail.com>
2024-05-11 17:23:18 -0400

    fuckin close it

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.UIA3;
+using FlaUI.UIA3.EventHandlers;
 
 namespace KeyAll
 {
@@ -7,7 +11,13 @@ namespace KeyAll
     {
         static void Main(string[] args)
         {
-            Application.Launch("Notepad.exe");
+            var application = FlaUI.Core.Application.Launch("Notepad.exe");
+
+            var mainWindow = application.GetMainWindow(new UIA3Automation());
+            ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+
+            mainWindow.FindFirstDescendant(cf.ByName("Close")).AsButton().Click();
+
         }
     }
 }
