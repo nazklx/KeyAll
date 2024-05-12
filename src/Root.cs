8552f3c50a041a584cdbb480ac32ad8b0f09e5f4
nazklx <nazklx13@gmail.com>
2024-05-12 02:03:04 -0400

    Jumping off from https://stackoverflow.com/questions/3654787/global-hotkey-in-console-application for keystrokes, might need to do this a different way to preserve certain keystrokes already used in other applications.

--- src/Root.cs
+++ src/Root.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 using FlaUI.Core;
 using FlaUI.Core.AutomationElements;
 using FlaUI.Core.Conditions;
@@ -12,18 +13,31 @@ namespace KeyAll.core
     {
         static void Main(string[] args)
         {
-            // Initialize other classes
-            Hkd hkdaemon = new Hkd();
-            Tbm taskbarmenu = new Tbm();
+            // Register keystroke, create event
+            Hkd.RegisterHotKey(Keys.A, KeyModifiers.Alt);
+            Hkd.HotKeyPressed += new EventHandler<HotKeyEventArgs>(HotKeyManager_HotKeyPressed);
+            Console.ReadLine();
+
 
             // Open notepad, find close button and press it
-            var application = Application.Launch("Notepad.exe");
+            var application = FlaUI.Core.Application.Launch("Notepad.exe");
 
             var mainWindow = application.GetMainWindow(new UIA3Automation());
             ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
 
             mainWindow.FindFirstDescendant(cf.ByName("Close")).AsButton().Click();
 
+        }
+        static void HotKeyManager_HotKeyPressed(object sender, HotKeyEventArgs e)
+        {
+            // Go through HotKeyEventArgs to see what keystroke was pressed and execute code
+            switch (e.Key)
+            {
+                case Keys.A when e.Modifiers == KeyModifiers.Alt:
+                    System.Windows.Forms.MessageBox.Show("Bam");
+                    Environment.Exit(0);
+                    break;
+            }
         }
     }
 }
